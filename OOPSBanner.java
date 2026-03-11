9b3b2b8013aa0c0817c15f75762278154dd5f10a
rk2779 <rk2779@srmist.edu.in>
2026-03-11 13:55:29 +0530

    Added UC6 static helper methods

---
+++ OOPSBanner.java
@@ -0,0 +1,56 @@
+
+
+public class OOPSBanner {
+
+    // Method for letter O
+    public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getSPattern() {
+        return new String[] {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        // Print OOPS
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        }
+
+    }
+}
